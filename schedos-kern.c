c8fe0c43bcc3390685b2031eb3e355a4b2f7ec5f
XIUXIUXIU5 <shaolei@ucla.edu>
2015-03-09 20:14:28 -0700

    4

--- schedos-kern.c
+++ schedos-kern.c
@@ -221,12 +221,12 @@ schedule(void)
 		int highest = ~(1 << 31);
 
 		int i;
-		
-
-		while (1) {
-			for (i = 1; i < NPROCS; i++)
+		for (i = 1; i < NPROCS; i++)
 			if (proc_array[i].p_state == P_RUNNABLE && proc_array[i].p_priority < highest) 
 				highest = proc_array[i].p_priority;
+
+		while (1) {
+			
 			// start at next pid to alternate b/w highest priority processes
 			pid = (pid + 1) % NPROCS;
 			if (proc_array[pid].p_state == P_RUNNABLE && proc_array[pid].p_priority == highest)
